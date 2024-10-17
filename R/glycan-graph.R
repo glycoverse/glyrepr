c5e6f9b57d9926b55f99308fe0b06c480017fd1b
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2024-10-17 21:30:22 +0800

    refactor: refactor glycan-graph.R

--- R/glycan-graph.R
+++ R/glycan-graph.R
@@ -7,56 +7,69 @@ new_glycan_graph <- function(graph) {
 validate_glycan_graph <- function(glycan) {
   stopifnot(inherits(glycan, "glycan_graph"))
   # Check if it is a directed graph
-  if (!igraph::is_directed(glycan)) {
+  if (!is_directed_graph(glycan)) {
     rlang::abort("Glycan graph must be directed.")
   }
   # Check if it is an out tree
-  if (!igraph::is_tree(glycan, mode = "out")) {
+  if (!is_out_tree(glycan)) {
     rlang::abort("Glycan graph must be an out tree.")
   }
   # Check if graph has a vertex attribute "mono"
   # This is the monosaccharide name, e.g. "GlcNAc", "Man", etc.
-  if (is.null(igraph::V(glycan)$mono)) {
+  if (!has_vertex_attribute(glycan, "mono")) {
     rlang::abort("Glycan graph must have a vertex attribute 'mono'.")
   }
   # Check if no NA in vertex attribute "mono"
-  if (any(is.na(igraph::V(glycan)$mono))) {
+  if (na_in_vertex_attribute(glycan, "mono")) {
     rlang::abort("Glycan graph must have no NA in vertex attribute 'mono'.")
   }
   # Check if all monosaccharides are known
-  if (!all(is_known_mono(igraph::V(glycan)$mono))) {
+  if (!all(is_known_mono(igraph::vertex_attr(glycan, "mono")))) {
     unknown_monos <- unique(igraph::V(glycan)$mono[!is_known_mono(igraph::V(glycan)$mono)])
-    abort_unknown_mono(unknown_monos)
+    msg <- glue::glue("Unknown monosaccharide: {stringr::str_c(unknown_monos, collapse = ', ')}")
+    rlang::abort(msg, monos = unknown_monos)
   }
   # Check if graph has an edge attribute "linkage"
-  if (is.null(igraph::E(glycan)$linkage)) {
+  if (!has_edge_attribute(glycan, "linkage")) {
     rlang::abort("Glycan graph must have an edge attribute 'linkage'.")
   }
   # Check if all linkages are valid
-  if (!all(is_valid_linkage(igraph::E(glycan)$linkage))) {
+  if (!all(is_valid_linkage(igraph::edge_attr(glycan, "linkage")))) {
     invalid_linkages <- unique(igraph::E(glycan)$linkage[!is_valid_linkage(igraph::E(glycan)$linkage)])
-    abort_invalid_linkage(invalid_linkages)
+    msg <- glue::glue("Invalid linkage: {stringr::str_c(invalid_linkages, collapse = ', ')}")
+    rlang::abort(msg, linkages = invalid_linkages)
   }
 }
 
 
-abort_unknown_mono <- function(monos) {
-  monos_str <- paste0(monos, collapse = ", ")
-  msg <- glue::glue("Unknown monosaccharide: {monos_str}")
-  rlang::abort("error_bad_mono", message = msg, monos = monos)
+is_directed_graph <- function(graph) {
+  igraph::is_directed(graph)
+}
+
+
+is_out_tree <- function(graph) {
+  igraph::is_tree(graph, mode = "out")
+}
+
+
+has_vertex_attribute <- function(graph, attr) {
+  attr %in% igraph::vertex_attr_names(graph)
+}
+
+
+na_in_vertex_attribute <- function(graph, attr) {
+  any(is.na(igraph::vertex_attr(graph, attr)))
+}
+
+
+has_edge_attribute <- function(graph, attr) {
+  attr %in% igraph::edge_attr_names(graph)
 }
 
 
 is_known_mono <- function(monos) {
   known_monos <- c(unique(monosaccharides$generic), monosaccharides$concrete)
   monos %in% known_monos
-}
-
-
-abort_invalid_linkage <- function(linkages) {
-  linkages_str <- paste0(linkages, collapse = ", ")
-  msg <- glue::glue("Invalid linkage: {linkages_str}")
-  rlang::abort("error_bad_linkage", message = msg, linkages = linkages)
 }
 
 
--- tests/testthat/test-glycan-graph.R
+++ tests/testthat/test-glycan-graph.R
@@ -69,7 +69,6 @@ test_that("validating one non-existing monosaccharide", {
 
   expect_error(validate_glycan_graph(glycan))
   err <- rlang::catch_cnd(validate_glycan_graph(glycan))
-  expect_s3_class(err, "error_bad_mono")
   expect_equal(err$message, "Unknown monosaccharide: Bad")
   expect_equal(err$monos, "Bad")
 })
@@ -110,7 +109,6 @@ patrick::with_parameters_test_that("validating bad linkage", {
 
     expect_error(validate_glycan_graph(glycan))
     err <- rlang::catch_cnd(validate_glycan_graph(glycan))
-    expect_s3_class(err, "error_bad_linkage")
   },
   bad_linkage = c("1-4", "c1-4", "b1", "abc", ""),
   .test_name = bad_linkage
