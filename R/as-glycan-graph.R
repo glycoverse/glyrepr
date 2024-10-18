5f62794b10a096d7ce6289e807868944c398f205
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2024-10-18 22:14:14 +0800

    feat: add `as_glycan_graph()`

---
+++ R/as-glycan-graph.R
@@ -0,0 +1,142 @@
+#' Convert igraph Graph to Glycan Graph
+#'
+#' @description
+#' A glycan graph is a subclass of igraph graph with additional constraints.
+#' This function checks these constraints and append the S3 class `glycan_graph`
+#' to the graph object.
+#'
+#' Two types of glycan graphs are supported: dual-node (DN) and node-edge (NE).
+#' A NE glycan graph is a directed modeling of a glycan structure,
+#' where nodes are monosaccharides and edges are linkages.
+#' A DN glycan graph is another way to model a glycan structure,
+#' where both monosaccharides and linkages are nodes.
+#' Monosaccharides and linkages alternate in a DN glycan graph.
+#' For more details, see "details" section.
+#'
+#' `as_dn_glycan_graph()` is the same as `as_glycan_graph()` with `type = "dn"`.
+#' `as_ne_glycan_graph()` is the same as `as_glycan_graph()` with `type = "ne"`.
+#'
+#' @details
+#' # S3 Classes
+#'
+#' A glycan graph is merely an igraph graph with an additional S3 class `glycan_graph`.
+#' Based on type of the glycan, `ne_glycan_graph` or `dn_glycan_graph` is also
+#' appended to the graph.
+#' Therefore, `sloop::s3_class()` of a glycan graph is either
+#' `c("ne_glycan_graph", "glycan_graph", "igraph")` or
+#' `c("dn_glycan_graph", "glycan_graph", "igraph")`.
+#'
+#' # Node-Edge (NE) Glycan Graph
+#'
+#' This is the most intuitive way to model a glycan structure.
+#' Nodes represent monosaccharides and edges represent linkages.
+#'
+#' Constraints:
+#' - The graph must be directed and an outward tree (reducing end as root).
+#' - The graph must have a vertex attribute `mono` for monosaccharide names.
+#' - The graph must have an edge attribute `linkage` for linkages.
+#' - Monosaccharide name must be known, either generic (Hex, HexNAc, etc.)
+#'   or concrete (Glc, Gal, etc.), but not a mixture of both.
+#'   NA is not allowed.
+#' - Linkages must be valid, in the form of "a/bX-Y", where X and Y are integers,
+#'   e.g. "b1-4", "a2-3", etc.
+#'   NA is allowed for unknown linkages.
+#'
+#' # Dual-Node (DN) Glycan Graph
+#'
+#' This is another way to model a glycan structure.
+#' In this mode, both monosaccharides and linkages are nodes,
+#' and they alternate in the graph to represent the structure.
+#' Python library `glycowork` and `glypy` use this model.
+#' It is not that intuitive, but allows a more flexible motif matching.
+#'
+#' Constraints:
+#' - The graph must be directed and an outward tree (reducing end as root).
+#' - The graph must have vertex attributes `type`, `mono`, and `linkage`.
+#'   `type` should be either "mono" or "linkage".
+#'   NA is not allowed for `type`.
+#'   `mono` and `linkage` are monosaccharide name and linkage, respectively.
+#'   For nodes with `type = "mono"`, `linkage` is always NA.
+#'   For nodes with `type = "linkage"`, `mono` is always NA.
+#' - Monosaccharide name must be known, either generic or concrete, but not a mixture of both.
+#'   For nodes with `type = "mono"`, NA is not allowed for `mono`.
+#' - Linkages must be valid.
+#'   For nodes with `type = "linkage"`, NA is allowed for `linkage`.
+#' - `mono` nodes and `linkage` nodes must alternate in the graph.
+#'   In another word, each edge must connect a `mono` node and a `linkage` node.
+#' - The outermost nodes (nodes with out-degree 0) must be `mono` nodes,
+#'   not `linkage` nodes.
+#'
+#' @param graph An igraph graph object.
+#' @param type A character string specifying the glycan graph type.
+#'   It can be "auto", "ne", or "dn".
+#'   If "auto" (the default), the function tries to infer the type,
+#'   and raises an error if it cannot.
+#'   If "ne" or "dn", the function assumes the graph is of the specified type.
+#'
+#' @return A glycan graph object.
+#'
+#' @examples
+#' library(igraph)
+#'
+#' # A simple NE glycan graph: GlcNAc(b1-4)GlcNAc
+#' graph <- make_graph(~ 1-+2)  # 1 and 2 are monosaccharides
+#' V(graph)$mono <- c("GlcNAc", "GlcNAc")
+#' E(graph)$linkage <- "b1-4"
+#' as_glycan_graph(graph)
+#'
+#' # A simple DN glycan graph: GlcNAc(b1-4)GlcNAc
+#' graph <- make_graph(~ 1-+2, 2-+3)  # 1 and 3 are monosaccharides, 2 is linkage
+#' V(graph)$type <- c("mono", "linkage", "mono")
+#' V(graph)$mono <- c("GlcNAc", NA, "GlcNAc")
+#' V(graph)$linkage <- c(NA, "b1-4", NA)
+#' as_glycan_graph(graph, type = "dn")
+#'
+#' @export
+as_glycan_graph <- function(graph, type = "auto") {
+  stopifnot(igraph::is_igraph(graph))
+  stopifnot(type %in% c("auto", "ne", "dn"))
+
+  if (type == "auto") {
+    glycan <- new_dn_glycan_graph(graph)
+    tryCatch(
+      validate_dn_glycan_graph(glycan),
+      error = function(e) {
+        glycan <- new_ne_glycan_graph(graph)
+        tryCatch(
+          validate_ne_glycan_graph(glycan),
+          error = function(e) {
+            rlang::abort(paste0(
+              "Could not infer glycan graph type. ",
+              "Please specify 'type' argument manually to ",
+              "see potential problems in the graph."))
+          }
+        )
+      }
+    )
+  } else if (type == "ne") {
+    glycan <- new_ne_glycan_graph(graph)
+    validate_ne_glycan_graph(glycan)
+  } else {  # type == "dn"
+    glycan <- new_dn_glycan_graph(graph)
+    validate_dn_glycan_graph(glycan)
+  }
+}
+
+
+#' @rdname as_glycan_graph
+#' @export
+as_dn_glycan_graph <- function(graph) {
+  stopifnot(igraph::is_igraph(graph))
+  glycan <- new_dn_glycan_graph(graph)
+  validate_dn_glycan_graph(glycan)
+}
+
+
+#' @rdname as_glycan_graph
+#' @export
+as_ne_glycan_graph <- function(graph) {
+  stopifnot(igraph::is_igraph(graph))
+  glycan <- new_ne_glycan_graph(graph)
+  validate_ne_glycan_graph(glycan)
+}
---
+++ tests/testthat/test-as-glycan-graph.R
@@ -0,0 +1,57 @@
+good_dn_graph <- function() {
+  graph <- igraph::make_graph(~ 1-+2, 2-+3)
+  igraph::V(graph)$type <- c("mono", "linkage", "mono")
+  igraph::V(graph)$mono <- c("Glc", NA, "Glc")
+  igraph::V(graph)$linkage <- NA_character_
+  graph
+}
+
+
+good_ne_graph <- function() {
+  graph <- igraph::make_graph(~ 1-+2, 2-+3)
+  igraph::V(graph)$mono <- c("Glc", "Gal", "Glc")
+  igraph::E(graph)$linkage <- NA_character_
+  graph
+}
+
+
+test_that("as_dn_glycan_graph works", {
+  glycan <- as_dn_glycan_graph(good_dn_graph())
+  expect_s3_class(glycan, c("dn_glycan_graph", "glycan_graph", "igraph"))
+})
+
+
+test_that("as_ne_glycan_graph works", {
+  glycan <- as_ne_glycan_graph(good_ne_graph())
+  expect_s3_class(glycan, c("ne_glycan_graph", "glycan_graph", "igraph"))
+})
+
+
+test_that("as_glycan_graph works for DN graphs", {
+  glycan <- as_glycan_graph(good_dn_graph(), type = "dn")
+  expect_s3_class(glycan, c("dn_glycan_graph", "glycan_graph", "igraph"))
+})
+
+
+test_that("as_glycan_graph works for NE graphs", {
+  glycan <- as_glycan_graph(good_ne_graph(), type = "ne")
+  expect_s3_class(glycan, c("ne_glycan_graph", "glycan_graph", "igraph"))
+})
+
+
+test_that("as_glycan_graph works for DN graphs (auto)", {
+  dn_glycan <- as_glycan_graph(good_dn_graph())
+  expect_s3_class(dn_glycan, c("dn_glycan_graph", "glycan_graph", "igraph"))
+})
+
+
+test_that("as_glycan_graph works for NE graphs (auto)", {
+  ne_glycan <- as_glycan_graph(good_ne_graph())
+  expect_s3_class(ne_glycan, c("ne_glycan_graph", "glycan_graph", "igraph"))
+})
+
+
+test_that("as_glycan_graph fails for invalid graphs", {
+  bad_graph <- igraph::make_graph(~ 1-+2, 2-+3, 3-+1)
+  expect_error(as_glycan_graph(bad_graph), "Could not infer glycan graph type")
+})
