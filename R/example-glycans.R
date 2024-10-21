f127b3e7f1002c2693838eae541282af12558322
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2024-10-21 22:52:03 +0800

    refactor: refactor `n_glycan_core()`

--- R/example-glycans.R
+++ R/example-glycans.R
@@ -35,22 +35,21 @@ n_glycan_core <- function(mode = "ne", linkage = TRUE, mono_type = "concrete") {
   if (!is.logical(linkage) && length(linkage) != 1) {
     rlang::abort("Linkage must be a single logical.")
   }
-  switch(
+  glycan <- switch(
     mode,
     ne = n_glycan_core_ne(linkage, mono_type),
     dn = n_glycan_core_dn(linkage, mono_type),
   )
+  if (mono_type %in% c("simple", "generic")) {
+    glycan <- convert_glycan_mono_type(glycan, mono_type)
+  }
+  glycan
 }
 
 
 n_glycan_core_ne <- function(linkage, mono_type) {
   graph <- igraph::make_graph(~ 1-+2, 2-+3, 3-+4, 3-+5)
-  igraph::V(graph)$mono <- switch(
-    mono_type,
-    simple = c("N", "N", "H", "H", "H"),
-    generic = c("HexNAc", "HexNAc", "Hex", "Hex", "Hex"),
-    concrete = c("GlcNAc", "GlcNAc", "Man", "Man", "Man")
-  )
+  igraph::V(graph)$mono <- c("GlcNAc", "GlcNAc", "Man", "Man", "Man")
   linkages <- c("b1-4", "b1-4", "a1-3", "a1-6")
   igraph::E(graph)$linkage <- if (linkage) linkages else NA_character_
   new_ne_glycan_graph(graph)
@@ -60,12 +59,7 @@ n_glycan_core_ne <- function(linkage, mono_type) {
 n_glycan_core_dn <- function(linkage, mono_type) {
   graph <- igraph::make_graph(~ 1-+2, 2-+3, 3-+4, 4-+5, 5-+6, 6-+7, 5-+8, 8-+9)
   igraph::V(graph)$type <- c("mono", rep(c("linkage", "mono"), 4))
-  igraph::V(graph)$mono <- switch(
-    mono_type,
-    simple = c("N", NA, "N", NA, "H", NA, "H", NA, "H"),
-    generic = c("HexNAc", NA, "HexNAc", NA, "Hex", NA, "Hex", NA, "Hex"),
-    concrete = c("GlcNAc", NA, "GlcNAc", NA, "Man", NA, "Man", NA, "Man")
-  )
+  igraph::V(graph)$mono <- c("GlcNAc", NA, "GlcNAc", NA, "Man", NA, "Man", NA, "Man")
   linkages <- c(NA, "b1-4", NA, "b1-4", NA, "a1-3", NA, "a1-6", NA)
   igraph::V(graph)$linkage <- if (linkage) linkages else NA_character_
   new_dn_glycan_graph(graph)
