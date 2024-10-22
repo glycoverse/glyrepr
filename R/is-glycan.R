76472c76890b5e89113d2e38e49d3eeadf7ff16b
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2024-10-22 09:52:50 +0800

    feat: add `is_ne_glycan()` and `is_dn_glycan()`

--- R/is-glycan.R
+++ R/is-glycan.R
@@ -1,7 +1,10 @@
 #' Check if an Object is a Glycan Graph
 #'
-#' This function checks if an object is a glycan graph.
-#' That is an object of class `glycan_graph`.
+#' These functions check if an object is a glycan graph
+#' (an object of class `glycan_graph`),
+#' a node-edge glycan graph (an object of class `ne_glycan_graph`),
+#' or a dual-node glycan graph (an object of class `dn_glycan_graph`).
+#' See docs for [as_glycan_graph()] for more details.
 #'
 #' @param x An object to be checked.
 #'
@@ -9,4 +12,18 @@
 #' @export
 is_glycan <- function(x) {
   inherits(x, "glycan_graph")
+}
+
+
+#' @rdname is_glycan
+#' @export
+is_ne_glycan <- function(x) {
+  inherits(x, "ne_glycan_graph")
+}
+
+
+#' @rdname is_glycan
+#' @export
+is_dn_glycan <- function(x) {
+  inherits(x, "dn_glycan_graph")
 }
--- tests/testthat/test-is-glycan.R
+++ tests/testthat/test-is-glycan.R
@@ -8,3 +8,15 @@ test_that("is_glycan not works for others", {
   x <- igraph::make_empty_graph()
   expect_false(is_glycan(x))
 })
+
+
+test_that("is_ne_glycan works", {
+  x <- new_ne_glycan_graph(igraph::make_empty_graph())
+  expect_true(is_ne_glycan(x))
+})
+
+
+test_that("is_dn_glycan works", {
+  x <- new_dn_glycan_graph(igraph::make_empty_graph())
+  expect_true(is_dn_glycan(x))
+})
