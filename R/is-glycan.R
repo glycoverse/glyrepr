323b67b842e4ca3367481a84f868142bbab3b134
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2024-10-27 13:25:08 +0800

    feat: add `glycan_graph_mode()`

--- R/is-glycan.R
+++ R/is-glycan.R
@@ -27,3 +27,21 @@ is_ne_glycan <- function(x) {
 is_dn_glycan <- function(x) {
   inherits(x, "dn_glycan_graph")
 }
+
+
+#' Determine the Glycan Graph Mode
+#'
+#' These functions determine the mode of a glycan graph.
+#' The mode can be either "ne" (node-edge) or "dn" (dual-node).
+#' See docs for [as_glycan_graph()] for more details.
+#'
+#' @param x A glycan graph.
+#'
+#' @return A character string.
+#' @export
+glycan_graph_mode <- function(x) {
+  if (!is_glycan(x)) {
+    stop("Input must be a glycan graph.")
+  }
+  if (is_ne_glycan(x)) "ne" else "dn"
+}
--- tests/testthat/test-is-glycan.R
+++ tests/testthat/test-is-glycan.R
@@ -20,3 +20,15 @@ test_that("is_dn_glycan works", {
   x <- new_dn_glycan_graph(igraph::make_empty_graph())
   expect_true(is_dn_glycan(x))
 })
+
+
+test_that("glycan_graph_mode works for NE glycans", {
+  glycan <- o_glycan_core_1(mode = "ne")
+  expect_equal(glycan_graph_mode(glycan), "ne")
+})
+
+
+test_that("glycan_graph_mode works for DN glycans", {
+  glycan <- o_glycan_core_1(mode = "dn")
+  expect_equal(glycan_graph_mode(glycan), "dn")
+})
